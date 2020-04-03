41f8b036c0b95db43894e100145e68a5e6954ee1
taoqf <tao_qiufeng@126.com>
2020-04-03 08:31:19 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # 扫码
+
+[微信官方文档](https://developers.weixin.qq.com/miniprogram/dev/api/wx.scanCode.html)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 
-export default function awx8(param1: string) {
+/**
+ * 调起客户端扫码界面进行扫码
+ * @see [微信官方文档](https://developers.weixin.qq.com/miniprogram/dev/api/wx.scanCode.html)
+ * @param onlyFromCamera [boolean] 是否只能从相机扫码，不允许从相册选择图片
+ */
+export default function scan_code(onlyFromCamera?: boolean) {
+	return new Promise<wx.ScanCodeData>((resolve, reject) => {
+		wx.scanCode({
+			onlyFromCamera,				// 是否只能从相机扫码，不允许从相册选择图片
+			fail(err) {
+				reject(err);
+			},
+			success(res) {
+				resolve(res);
+			}
+		});
+	});
 }
